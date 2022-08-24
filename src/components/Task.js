949e403ce3ab8ddf4a257933854f23ef49ae10fd
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2022-08-24 10:34:03 -0400

    step: simple component

---
+++ src/components/Task.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+
+export default function Task({task:{id,title,state}},onArchiveTask, onPinTask){
+return (
+    <div className={`list-item ${state}`}>
+        <label 
+            htmlFor="checked" 
+            aria-label={`archivedTask-${id}`} 
+            className="checkbox"
+        >
+            <input 
+                type="checkbox" 
+                name="checked" 
+                id={`archivedTask-${id}`}
+                disabled={true}
+                checked={state==='TASK_ARCHIVED'} 
+            />
+            <span className="checkbox-custom" onClick={()=>onArchiveTask(id)}/>
+        </label>
+
+        <label htmlFor="title" aria-label={title} className="title">
+            <input 
+                type="text" 
+                value={title} 
+                readOnly={true} 
+                name="title"
+                placeholder="Input Title" 
+            />
+        </label>
+
+        {state !== 'TASK_ARCHIVED' && (
+            <button 
+                className="pin-button" 
+                onClick={() => onPinTask(id)}
+                id={`pinTask-${id}`}
+                aria-label={`pinTask-${id}`}
+                key={`pinTask-${id}`}
+            >
+                <span className={`icon-star`}/>
+            </button>
+        )}
+    </div>
+);
+}
+
+Task.propTypes = {
+    /** Composition of the task */
+    task:PropTypes.shape({
+        /** Id of the task */
+        id: PropTypes.string.isRequired,
+        /** Title of the task */
+        title: PropTypes.string.isRequired,
+        /** Current state of the task */
+        state: PropTypes.string.isRequired,
+    }),
+    /** Event to change the task to archived */
+    onArchiveTask: PropTypes.func,
+    /** Evento to change the task to pinned */
+    onPinTask: PropTypes.func,
+};
